ff18dca7952969982a2c5c739aed940806573224
Nabi Karampoor <thisisnabi@outlook.com>
2024-07-14 19:38:53 +0330

    Add global Indexs

---
+++ src/Media/Infrastructure/MediaDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Media.Infrastructure
+{
+     
+    public class MediaDbContext : DbContext
+    {
+        public MediaDbContext(DbContextOptions<MediaDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<UrlToken> Tokens { get; set; }
+    }
+}
+
+
+public class UrlToken
+{
+    public Guid Id { get; set; }
+
+    public required string BacketName { get; set; }
+
+    public required string ObjectName { get; set; }
+
+    public required string ContentType { get; set; }
+
+}
--- src/Media/Program.cs
+++ src/Media/Program.cs
@@ -2,6 +2,8 @@ using MassTransit;
 using Media.Infrastructure;
 using Media.Infrastructure.IntegrationEvents;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Minio;
 using Minio.DataModel.Args;
 
@@ -9,6 +11,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MediaDbContext>(configure =>
+{
+    configure.UseInMemoryDatabase("test");
+});
 
 builder.BrokerConfiure();
 
@@ -27,6 +34,7 @@ app.MapPost("/{backet_name}/{catalog_id}", async (
     [FromRoute(Name = "backet_name")] string backetName,
     [FromRoute(Name = "catalog_id")] string catalogId,
     IFormFile file,
+    MediaDbContext dbContext,
     IPublishEndpoint publisher,
     IConfiguration configuration) =>
 {
@@ -46,11 +54,24 @@ app.MapPost("/{backet_name}/{catalog_id}", async (
                                 .WithStreamData(file.OpenReadStream())
                                 .WithObjectSize(file.Length);
 
+
     try
     {
 
         await minio.PutObjectAsync(putObjectArgs);
-        var url = $"{endpoint}/{backetName}/{file.FileName}";
+
+        var token = new UrlToken
+        {
+            BacketName = backetName,
+            ObjectName = file.FileName,
+            ContentType = file.ContentType,
+            Id = Guid.NewGuid()
+        };
+
+        dbContext.Tokens.Add(token);
+        await dbContext.SaveChangesAsync();
+
+        var url = $"https://localhost:7030/{token.Id}";
         await publisher.Publish(new MediaUploadedEvent(file.FileName, url, catalogId, DateTime.UtcNow));
     }
     catch (Exception)
@@ -60,5 +81,37 @@ app.MapPost("/{backet_name}/{catalog_id}", async (
 
 
 }).DisableAntiforgery();
+
+
+app.MapGet("/{token:guid:required}", async (
+      MediaDbContext dbContext,
+       IConfiguration configuration,
+    Guid Token) => {
+
+        var foundToken = await dbContext.Tokens.FirstOrDefaultAsync(x => x.Id == Token);
+
+        if (foundToken is null)
+            throw new InvalidOperationException();
+
+        var endpoint = configuration["MinioStorage:MinioEndpoint"];
+        var accessKey = configuration["MinioStorage:AccessKey"];
+        var secretKey = configuration["MinioStorage:SecretKey"];
+        var minio = new MinioClient()
+                            .WithEndpoint(endpoint)
+                            .WithCredentials(accessKey, secretKey)
+                            .Build();
+
+        var memoeyStream = new MemoryStream(); 
+        GetObjectArgs getObjectArgs = new GetObjectArgs()
+                                   .WithBucket(foundToken.BacketName)
+                                   .WithObject(foundToken.ObjectName)
+                                   .WithCallbackStream((stream) =>
+                                   {
+                                       stream.CopyTo(memoeyStream);
+                                   });
+
+        await minio.GetObjectAsync(getObjectArgs);
+        return Results.File(memoeyStream.ToArray(),contentType: foundToken.ContentType);
+    });
 
 app.Run();
